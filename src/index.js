107ab7e6d77f9cf83eb24f7d94a6db8ccef2dc6e
CristianG540 <cristianneatproy@gmail.com>
2018-02-19 19:02:12 -0500

    agrego una nueva funcionalidad que de cuando en cuando sincronisa sap con couchdb por si algun desface se presenta

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 import Promise from 'bluebird'
+import _ from 'lodash'
+import Papa from 'papaparse'
 import { originWins } from './modules/deconflict'
 import { env } from './modules/config'
+import { LookForDiffs } from './modules/lookForDiffs'
 
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
 const nano = require('nano')(`https://${env.db.user}:${env.db.pass}@${env.db.url}`)
-const remoteDB = nano.db.use('producto')
+const remoteDB = nano.db.use('producto_1')
+
+let lookForDiffs = new LookForDiffs()
 
 // create Promise-compatible versions of all functions
 Promise.promisifyAll(remoteDB)
 
+setInterval(() => {
+  let fileStream = fs.createReadStream(path.resolve(os.tmpdir(), 'fz3temp-3', 'product.txt'))
+  Papa.parse(fileStream, {
+    header: true,
+    complete: csvParsed => {
+      remoteDB.listAsync({ include_docs: true }).then(res => {
+        lookForDiffs.checkAndResolve(_.filter(csvParsed.data, ['_delete', 'false']), _.map(res.rows, 'doc')).then(res => {
+          console.log('prods actualizados', res)
+        }).catch(err => {
+          console.error('Puto error no previsto', err)
+        })
+      }).catch(err => {
+        console.error('Error al listar los documentos', err)
+      })
+      fileStream.destroy()
+    },
+    error: err => {
+      console.error('Puto error', err)
+      fileStream.destroy()
+    }
+  })
+}, 600000)
+
+// console.log(path.resolve(os.tmpdir(), 'fz3temp-3', 'product.txt'))
 setInterval(() => {
   remoteDB.viewAsync('conflicts', 'all-conflicts-view', {
     limit: 30
@@ -24,4 +56,4 @@ setInterval(() => {
   }).catch(err => {
     console.error('Error alldocs', err)
   })
-}, 40000)
+}, 20000)
---
+++ src/modules/lookForDiffs.js
@@ -0,0 +1,54 @@
+import Promise from 'bluebird'
+import _ from 'lodash'
+import { env } from './config'
+const nano = require('nano')(`https://${env.db.user}:${env.db.pass}@${env.db.url}`)
+const remoteDB = nano.db.use('producto_1')
+// create Promise-compatible versions of all functions
+Promise.promisifyAll(remoteDB)
+
+export class LookForDiffs {
+  constructor () {
+    this.csvProds = []
+    this.couchProds = []
+  }
+
+  binarySearch (arr, property, search) {
+    let low = 0
+    let high = arr.length
+    let mid
+    while (low < high) {
+      mid = (low + high) >>> 1 // faster version of Math.floor((low + high) / 2)
+      arr[mid][property] < search ? low = mid + 1 : high = mid
+    }
+    return low
+  }
+
+  checkAndResolve (csvProds, couchProds) {
+    return new Promise((resolve, reject) => {
+      let prodstoUpdate = []
+      _.each(couchProds, couchProd => {
+        let iCsvProd = this.binarySearch(csvProds, 'codigo', couchProd._id)
+        if (_.has(csvProds[iCsvProd], 'codigo') && csvProds[iCsvProd].codigo === couchProd._id) {
+          if (couchProd.existencias !== parseInt(csvProds[iCsvProd].cantInventario, 10)) {
+            couchProd.origen = 'sap'
+            couchProd.updated_at = Date.now()
+            couchProd.existencias = parseInt(csvProds[iCsvProd].cantInventario, 10)
+
+            prodstoUpdate.push(couchProd)
+          }
+        } else {
+          if (couchProd._id.split('/')[0] !== '_design') {
+            couchProd._deleted = true
+            prodstoUpdate.push(couchProd)
+          }
+        }
+      })
+
+      remoteDB.bulkAsync({ docs: prodstoUpdate }).then(res => {
+        resolve(res)
+      }).catch(err => {
+        reject(err)
+      })
+    })
+  }
+}
